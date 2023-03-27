f66784fc020a18e1a0e858a32e05c83b7bc2290f
FranciscoJunior12 <105241871+FranciscoJunior12@users.noreply.github.com>
2023-03-27 21:30:20 +0200

    Criação das secções projects e footer

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -4,3 +4,33 @@ const botao = document.querySelector('#down-arrow');
 botao.addEventListener('click', () => {
     window.scroll({ top: window.innerHeight, behavior: 'smooth' })
 })
+
+
+
+
+
+const observer = new IntersectionObserver(entries => {
+
+    
+
+    Array.from(entries).forEach(entry => {
+        if (entry.intersectionRatio >= 1) {
+
+            entry.target.classList.add('init-hidden-off')
+        }
+    })
+
+},
+    { threshold: 1 });
+
+Array.from(document.querySelectorAll('.init-hidden')).forEach(elemento => {
+
+    observer.observe(elemento);
+
+})
+
+
+
+
+
+
